c79a5251bc94c593e30ddd1f8b8c3a1ec5c3aa11
Hugo Marcelo <hugo.silva@arquivei.com.br>
2021-01-25 18:27:45 -0300

    feat: first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '64d3ef2ca72f3338434a34287811962f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6539e22f-b5bb-49e9-8daa-2fc79fcd21d5'
+};
